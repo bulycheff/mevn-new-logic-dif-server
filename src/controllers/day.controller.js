66d784c64aff94c3881610034f5ad8765d72ae2c
Sergey Bulychev <bulycheff@ya.ru>
2021-12-09 20:07:58 +0500

    added aggr request DayInfo to api

--- src/controllers/day.controller.js
+++ src/controllers/day.controller.js
@@ -4,6 +4,105 @@ const { Types } = require('mongoose')
 
 module.exports = {
 
+  async agrInfoAboutDayByDayId(req, res) {
+    try {
+      const id = req.params.id
+
+      const agr = [
+        {
+          '$match': {
+            '_id': {
+              '$eq': new Types.ObjectId(id)
+            }
+          }
+        }, {
+          '$unwind': {
+            'path': '$client',
+            'preserveNullAndEmptyArrays': true
+          }
+        }, {
+          '$lookup': {
+            'from': 'purchases',
+            'localField': 'client',
+            'foreignField': 'client_id',
+            'as': 'purchases'
+          }
+        }, {
+          '$lookup': {
+            'from': 'clients',
+            'localField': 'client',
+            'foreignField': '_id',
+            'as': 'client'
+          }
+        }, {
+          '$unwind': {
+            'path': '$client',
+            'preserveNullAndEmptyArrays': true
+          }
+        }
+      ]
+
+      let dayInfo = await Day.aggregate(agr)
+      dayInfo = dayInfo.map(item => {
+        let { purchases, ...all } = item
+        all.client.purchases = purchases
+        return all
+      })
+      let { client, ...info } = dayInfo[0]
+      info.clients = dayInfo.map(item => item.client)
+
+      let info1 = info.clients.reduce((acc, cur) => {
+        acc.clients += 1
+
+        let count = cur.purchases.reduce((acc, cur)=>{
+          if(cur.payment_category === 'program') {
+            acc.programs.value += 1
+            acc.programs.sum += cur.item_cost_discounted
+          } else if (cur.payment_category === 'bar') {
+            acc.bars.value += 1
+            acc.bars.sum += cur.item_cost_discounted
+          }
+          acc.total.value += 1
+          acc.total.sum += cur.item_cost_discounted
+
+          if((new Date(acc.last)).valueOf()<(new Date(cur.opened_at)).valueOf()) {
+            acc.last=cur.opened_at
+          }
+
+          return acc
+        }, {
+          last: acc.last,
+          programs: { value: 0, sum: 0 },
+          bars: { value: 0, sum: 0 },
+          total: { value: 0, sum: 0 }
+        })
+
+        acc.bars.value+=count.bars.value
+        acc.programs.value+=count.programs.value
+        acc.total.value+=count.total.value
+
+        acc.bars.sum+=count.bars.sum
+        acc.programs.sum+=count.programs.sum
+        acc.total.sum+=count.total.sum
+
+        acc.last=count.last
+
+        return acc
+      }, {
+        opened_at: info.opened_at,
+        last: info.opened_at,
+        clients: 0,
+        programs: { value: 0, sum: 0 },
+        bars: { value: 0, sum: 0 },
+        total: { value: 0, sum: 0 }
+      })
+
+      res.status(200).json(info1)
+    } catch (e) {
+      boom.boomify(e)
+    }
+  },
+
   async open({ user }, res) {
     try {
       const { iat, exp, id, ...userFields } = user
--- src/routes/day.js
+++ src/routes/day.js
@@ -3,6 +3,7 @@ const { dayController } = require('../controllers')
 const authMiddleware = require('../middleware/auth.middleware')
 const roleMiddleware = require('../middleware/role.middleware')
 
+router.get('/info/:id', authMiddleware, dayController.agrInfoAboutDayByDayId)
 router.post('/', authMiddleware, dayController.open)
 router.get('/', authMiddleware, dayController.getAllOpenedWithStatus)
 router.get('/:id', authMiddleware, dayController.getOne)
